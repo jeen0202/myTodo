5f32649b498765c45136681e473f98a093f8684d
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-09 17:02:14 +0900

    기본 Style 적용

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    res.send('auth page');
+})
+
+router.get('/login', (req,res,next)=>{
+    res.render('login');
+  });
+
+router.get('/register', (req,res,next)=>{
+    res.render('register');
+})
+module.exports = router;
