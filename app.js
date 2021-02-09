157eece885c892699b7797ca1af53c9d2bb11e17
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-09 17:52:49 +0900

    Login & Register 기능 구현

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//var flash = require('connect-flash');
+
+const readlow = require('./lib/readlow');
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+var passport = require('./lib/passport')(app);
+app.get('*',readlow.todolists);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'holly molly',
+  resave: false,
+  saveUninitialized: false,
+  store : new FileStore()
+}));   
+//app.use(flash());
 
 app.use('/', indexRouter);
-app.use('/auth', authRouter);
+app.use('/auth', authRouter(passport));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,59 @@
-var express = require('express');
+var db = require('../lib/lowdb');
+var nanoid = require('nanoid');
+var bcrypt = require('bcrypt');
+const express = require('express')
 var router = express.Router();
 
+module.exports = function(passport){
 router.get('/',(req,res,next)=>{
     res.send('auth page');
 })
 
 router.get('/login', (req,res,next)=>{
     res.render('login');
-  });
+});;
+router.post('/login_process',passport.authenticate('local',
+        {failureRedirect : '/auth/login'}),
+        (req, res) => {
+        req.session.save( () => {
+        res.redirect('/')
+        })
+});
 
 router.get('/register', (req,res,next)=>{
     res.render('register');
-})
-module.exports = router;
+});
+router.post('/register_process',(req,res)=>{     
+    var post = req.body;
+    var email = post.email;  
+    var pass1 = post.password1;
+    var pass2 = post.password2;
+    var displayName = post.displayName;
+    if(pass1 !== pass2){
+        //request.flash('error', 'Passwords must be same!');
+        response.redirect('/auth/register');          
+    }else{     
+        bcrypt.hash(pass1, 10, function(err, hash) {
+        var user = db.get('users').find({email:email}).value();
+        if(user){
+            user.password = hash;
+            user.displayName = displayName;
+            db.get('uesrs').find({id:user.id}).assign(user).write();
+        }else{
+            var user ={
+            id:nanoid.nanoid(),
+            email:email,
+            password:hash,
+            displayName:displayName
+            }
+        db.get('users').push(user).write();
+        }   
+        req.login(user, (err)=>{
+            return res.redirect('/')
+            })    
+        });
+    }          
+});
+return router;
+}
+
