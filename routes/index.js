f4416c6a39e3428c956b393723a21c42f7b73741
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-14 18:43:25 +0900

    보안 update

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var authstatus = false;
   var displayName = '';
-  // if(req.user){
-  //   authstatus = true;
-  //   displayName = req.user.displayName;
-  // }else{
-  //   authstatus = false;
-  // }
   res.render('index', { title: 'My TODO',authstatus:authstatus, displayName: displayName, toDolists:req.toDolists});
 });
+
 router.get('/:pageId',(req,res)=>{
   var displayName = req.user.displayName;
   console.log("toDolists : " ,req.toDolists);
